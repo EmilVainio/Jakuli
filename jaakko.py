d0a14bb04edc74ceea23d2e3869075e996f867a1
Emil Vainio <emil.vainio@hotmail.fi>
2022-05-06 02:57:43 +0300

    Update jaakko.py

--- jaakko.py
+++ jaakko.py
@@ -28,45 +28,160 @@ def root(numerot):
 def plusmiinus(teksti):
     
 
-    kirjaimet = [kirjain for kirjain in teksti]
-    
-    for i in kirjaimet:
+    cheese = []
+    merkit = [
+        '+', 
+        '-', 
+        '*', 
+        '/',
+        '^'
+    ]
+
+
+    karhu = teksti
+
+    for a in merkit:
+
+        karhu = karhu.split(a)
+
+        for i in range(len(karhu)-1):
+
+            karhu.insert(i*2+1,a)
+
+        karhu = ' '.join(karhu)
         
-        match i:
-            case '+':
-                luku = teksti.split('+')
-                for i in range(len(luku)-1):
-                    luku[i+1] = float(luku[i])+float(luku[i+1])
-                print(luku[-1])
-                break
-            case '-':
-                luku = teksti.split('-')
-                for i in range(len(luku)-1):
-                    luku[i+1] = float(luku[i])-float(luku[i+1])
-                print(luku[-1])
-                break
-            case '*':
-                luku = teksti.split('*')
-                for i in range(len(luku)-1):
-                    luku[i+1] = float(luku[i])*float(luku[i+1])
-                print(luku[-1])
-                break
-            case '/':
-                luku = teksti.split('/')
-                for i in range(len(luku)-1):
-                    luku[i+1] = float(luku[i])/float(luku[i+1])
-                print(luku[-1])
-                break
-            case '^':
-                luku = teksti.split('^')
-                for i in range(len(luku)-1, 0, -1):
-                    luku[i-1] = float(luku[i-1])**float(luku[i])
-                print(luku[0])
-                break
+
+    karhu = karhu.split(' ')
+
+    # for n in range(len(merkit)):
+    #     cheese.append(0)
+    #     for m in karhu:
+    #         if merkit[n] == m:
+    #             cheese[n] = int(cheese[n])+1
+        
+    # print(cheese)
+    cheese = karhu.copy()
+    
+    for b in range(len(karhu),0,-1):
+
+        if karhu[b-1] == '^':
+
+            karhu[b-2] = float(karhu[b-2])**float(karhu[b])
+
+            del karhu[b-1:b+1]
+    print(cheese)
+    for c in range(len(karhu)):
+        
+        print(c)
+        print(karhu[7])
+        if karhu[c] == '*':
+            karhu[c-1] = float(karhu[c-1])*float(karhu[c+1])
+            del karhu[c:c+2]
+        elif karhu[c] == '/':
+            karhu[c-1] = float(karhu[c-1])/float(karhu[c+1])
+            del karhu[c:c+2]
+
+    print(karhu)
+
+
+
+    
+    # kameli = []
+    
+    
+    # if '^' in teksti:
+    #     jaakko = teksti.split('^')
+        
+    
+    # jaettui = []
+
+    # for i in jaakko:
+       
+        
+
+    #     tuomas = []
+        
+    #     for a in merkit:
+            
+    #         if a in i:
+    #             tuomas.append(a)
+        
+
+        
+    #     # print(tuomas)
+    #     # print(len(tuomas))
+    #     if len(tuomas) > 0:
+
+    #         kahvi = []
+
+    #         # print(jaettui)
+    #         # print(jaakko.index(i))
+    #         for b in range(len(tuomas)):
+
+    #             jaettui.append(i.split(tuomas[b]))
+    #             jaettui[jaakko.index(i)-1].append(jaettui[jaakko.index(i)-1][1])
+    #             jaettui[jaakko.index(i)-1][1] = tuomas[b]
+    #         print(jaettui)
             
         
-    if len(luku) == 1:
-        print("\nJotain meni rikki!")
+    #         for c in range(len(jaettui)):
+
+    #             kahvi.append(len(jaettui[c][0])) 
+    #         # print('j')
+    #         # print(kahvi)
+            
+    #         kameli.append(jaettui[kahvi.index(min(kahvi))][0])
+    #         kameli.append(jaettui[kahvi.index(min(kahvi))][1])
+
+    #         if len(jaettui[kahvi.index(min(kahvi))]) > 2:
+
+    #             kameli.append(jaettui[kahvi.index(min(kahvi))][2])
+        
+    #     else: kameli.append(i)
+    
+    # print(kameli)
+
+    # kirjaimet = [kirjain for kirjain in teksti]
+    
+    # print(kirjaimet)
+
+    # for i in kirjaimet:
+        
+    #     match i:
+    #         case '+':
+    #             luku = teksti.split('+')
+    #             for i in range(len(luku)-1):
+    #                 luku[i+1] = float(luku[i])+float(luku[i+1])
+    #             print(luku[-1])
+    #             break
+    #         case '-':
+    #             luku = teksti.split('-')
+    #             for i in range(len(luku)-1):
+    #                 luku[i+1] = float(luku[i])-float(luku[i+1])
+    #             print(luku[-1])
+    #             break
+    #         case '*':
+    #             luku = teksti.split('*')
+    #             for i in range(len(luku)-1):
+    #                 luku[i+1] = float(luku[i])*float(luku[i+1])
+    #             print(luku[-1])
+    #             break
+    #         case '/':
+    #             luku = teksti.split('/')
+    #             for i in range(len(luku)-1):
+    #                 luku[i+1] = float(luku[i])/float(luku[i+1])
+    #             print(luku[-1])
+    #             break
+    #         case '^':
+    #             luku = teksti.split('^')
+    #             for i in range(len(luku)-1, 0, -1):
+    #                 luku[i-1] = float(luku[i-1])**float(luku[i])
+    #             print(luku[0])
+    #             break
+            
+        
+    # if len(luku) == 1:
+    #     print("\nJotain meni rikki!")
 
                 
             
@@ -91,7 +206,7 @@ def main():
             for i in lista:
                 print(lista[i])
         else:
-            try:
+            #try:
 
                 
                 if any(i in kauha for i in lista):
@@ -112,7 +227,7 @@ def main():
                     
                 
 
-            except: print("\nJotain meni rikki!")
+           # except: print("\nJotain meni rikki!")
         
 print("Kirjoita 'POIS' poistuaksesi.\n'APUA' komennolla näet komennot.\n")
 main()
